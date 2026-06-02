4521384be982d0903f3ddda2e34b6a0b49a11ec3
Pithak-00 <31059244+Pithak-00@users.noreply.github.com>
2026-06-02 13:28:46 +0900

    ローカル環境起動の修正

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-)s=wy#0ecpfx92^f^m8d-fy+@rzh+^(o7um4t$i1!3twyo+krm"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -12,4 +12,4 @@ fi
 
 python manage.py migrate
 
-exec gunicorn config.wsgi:application --bind 0.0.0.0:8000 --workers 3
+exec python manage.py runserver 0.0.0.0:8000
